13685950c2487d1f6f25ba1763dcccc974047249
Andrij Valkiv <andrij2012@gmail.com>
2013-07-17 18:30:04 +0300

    add new win combination and features that allow to draw board from 3x3 to 9x9, game start by computer

--- script.js
+++ script.js
@@ -5,14 +5,34 @@
         document.getElementById('canvas').onclick = movePlayer;
     };
 
-
+    var numOfCell;
+    var beginPlayer = true;
     var context;
     var xBoard = [];
     var oBoard = [];
     var width, height; // width and height of Board
-    var winCombinations =  [['0 0', '1 0', '2 0'], ['0 1', '1 1', '2 1'], ['0 2', '1 2', '2 2'],
-                            ['0 0', '0 1', '0 2'], ['1 0', '1 1', '1 2'], ['2 0', '2 1', '2 2'],
-                            ['0 0', '1 1', '2 2'], ['2 0', '1 1', '0 2']]; // winning combinations
+    var winCombinations =  [['0 0', '1 0', '2 0', '3 0', '4 0', '5 0', '6 0', '7 0', '8 0'],
+                            ['0 1', '1 1', '2 1', '3 1', '4 1', '5 1', '6 1', '7 1', '8 1'],
+                            ['0 2', '1 2', '2 2', '3 2', '4 2', '5 2', '6 2', '7 2', '8 2'],
+                            ['0 3', '1 3', '2 3', '3 3', '4 3', '5 3', '6 3', '7 3', '8 3'],
+                            ['0 4', '1 4', '2 4', '3 4', '4 4', '5 4', '6 4', '7 4', '8 4'],
+                            ['0 5', '1 5', '2 5', '3 5', '4 5', '5 5', '6 5', '7 5', '8 5'],
+                            ['0 6', '1 6', '2 6', '3 6', '4 6', '5 6', '6 6', '7 6', '8 6'],
+                            ['0 7', '1 7', '2 7', '3 7', '4 7', '5 7', '6 7', '7 7', '8 7'],
+                            ['0 8', '1 8', '2 8', '3 8', '4 8', '5 8', '6 8', '7 8', '8 8'],
+
+                            ['0 0', '0 1', '0 2', '0 3', '0 4', '0 5', '0 6', '0 7', '0 8'],
+                            ['1 0', '1 1', '1 2', '1 3', '1 4', '1 5', '1 6', '1 7', '1 8'],
+                            ['2 0', '2 1', '2 2', '2 3', '2 4', '2 5', '2 6', '2 7', '2 8'],
+                            ['3 0', '3 1', '3 2', '3 3', '3 4', '3 5', '3 6', '3 7', '3 8'],
+                            ['4 0', '4 1', '4 2', '4 3', '4 4', '4 5', '4 6', '4 7', '4 8'],
+                            ['5 0', '5 1', '5 2', '5 3', '5 4', '5 5', '5 6', '5 7', '5 8'],
+                            ['6 0', '6 1', '6 2', '6 3', '6 4', '6 5', '6 6', '6 7', '6 8'],
+                            ['7 0', '7 1', '7 2', '7 3', '7 4', '7 5', '7 6', '7 7', '7 8'],
+                            ['8 0', '8 1', '8 2', '8 3', '8 4', '8 5', '8 6', '8 7', '8 8'],
+
+                            ['0 0', '1 1', '2 2', '3 3', '4 4', '5 5', '6 6', '7 7', '8 8'],
+                            ['8 0', '7 1', '6 2', '5 3', '4 4', '3 5', '2 6', '1 7', '0 8']]; // winning combinations
 
 
 
@@ -23,18 +43,23 @@
      * |0 2|1 2|2 2|
      */
     function  paintBoard() {
-        var index       = document.getElementById('list').selectedIndex;
-        var numOfCell   = document.getElementsByTagName("option")[index].value;
+        // Get the selected element from select list
+        var index = document.getElementById('list').selectedIndex;
+        numOfCell = parseInt(document.getElementsByTagName("option")[index].value, 10);
 
         var board = document.getElementById('canvas');
 
+        // Hide the select list
         document.getElementById('cellNumber').style.display = "none";
 
+        // Display the board
         board.style.display = 'inline';
 
-        width   = parseInt(numOfCell, 10) * 100;
-        height  = parseInt(numOfCell, 10) * 100;
+        // Generate the width and the height of game board
+        width   = numOfCell * 100;
+        height  = numOfCell * 100;
         
+        // Width and height of canvas element
         board.height = height;
         board.width  = width;
         
@@ -45,20 +70,30 @@
         context.strokeStyle = "#000";
         context.lineWidth = 3;
 
-        context.moveTo((width / 3), 0);
-        context.lineTo((width / 3), height);
+        /**
+         * Generate the cells number
+         */
+        for(var i = 1; i < 9; i++) {
+            context.moveTo((width / numOfCell) * i, 0);
+            context.lineTo((width / numOfCell) * i, height);
 
-        context.moveTo((width / 3) * 2, 0);
-        context.lineTo((width / 3) * 2, height);
+            context.moveTo(0, i * (height / numOfCell));
+            context.lineTo(width, i * (height / numOfCell));
 
-        context.moveTo(0, (height / 3));
-        context.lineTo(width, (height / 3));
+            context.stroke();
+            if(i == (numOfCell -1)) {
+                break;
+            }
 
-        context.moveTo(0, 2 * (height / 3));
-        context.lineTo(width, 2 * (height / 3));
-
-        context.stroke();
+        }
         context.closePath();
+        if(beginPlayer) {
+            beginPlayer = false;
+        } else {
+            setTimeout(randomMoveComputer, 400);
+            beginPlayer = true;
+        }
+
     }
 
     /**
@@ -75,7 +110,7 @@
             context.beginPath();
             context.strokeStyle = '#FF0000';
 
-            var offsetX = (width / 3) * 0.1;
+            /*var offsetX = (width / 3) * 0.1;
             var offsetY = (height / 3) * 0.1;
 
             var beginX = x * (width / 3) + offsetX;
@@ -83,6 +118,15 @@
 
             var endX = (x + 1) * (width / 3) - offsetX * 2;
             var endY = (y + 1) * (height / 3) - offsetY * 2;
+            */
+            var offsetX = (width / numOfCell) * 0.1;
+            var offsetY = (height / numOfCell) * 0.1;
+
+            var beginX = x * (width / numOfCell) + offsetX;
+            var beginY = y * (height / numOfCell) + offsetY;
+
+            var endX = (x + 1) * (width / numOfCell) - offsetX * 2;
+            var endY = (y + 1) * (height / numOfCell) - offsetY * 2;
 
             context.moveTo(beginX, beginY);
             context.lineTo(endX, endY);
@@ -114,14 +158,14 @@
             context.beginPath();
             context.strokeStyle = '#0000FF';
 
-            var offsetX = (width / 3) * 0.1;
-            var offsetY = (height / 3) * 0.1;
+            var offsetX = (width / numOfCell) * 0.1;
+            var offsetY = (height / numOfCell) * 0.1;
 
-            var beginX = x * (width / 3) + offsetX;
-            var beginY = y * (height / 3) + offsetY;
+            var beginX = x * (width / numOfCell) + offsetX;
+            var beginY = y * (height / numOfCell) + offsetY;
 
-            var endX = (x + 1) * (width / 3) - offsetX * 2;
-            var endY = (y + 1) * (height / 3) - offsetY * 2;
+            var endX = (x + 1) * (width / numOfCell) - offsetX * 2;
+            var endY = (y + 1) * (height / numOfCell) - offsetY * 2;
 
             context.arc(beginX + ((endX - beginX) / 2), beginY + ((endY - beginY) / 2), (endX - beginX) / 2, 0, Math.PI * 2);
 
@@ -138,8 +182,8 @@
      * @param event
      */
     function movePlayer(e) {
-        var x = Math.floor(e.clientX / (width / 3));
-        var y = Math.floor(e.clientY / (height / 3));
+        var x = Math.floor(e.clientX / (width / numOfCell));
+        var y = Math.floor(e.clientY / (height / numOfCell));
 
         function timeOut() {
             if(oBoard.length == 0) {
@@ -164,8 +208,8 @@
      * Move of computer is random (first move etc.)
      */
     function randomMoveComputer() {
-        var x = Math.floor(Math.random() * 3);
-        var y = Math.floor(Math.random() * 3);
+        var x = Math.floor(Math.random() * numOfCell);
+        var y = Math.floor(Math.random() * numOfCell);
 
         if(!paintO(x, y)) {
             randomMoveComputer();
@@ -295,7 +339,7 @@
             } else if(oBoard.indexOf(a) != -1 && oBoard.indexOf(b) != -1 && oBoard.indexOf(c) != -1) {
                 alert("Computer win!");
                 return true;
-            } else if (xBoard.length == 5) {
+            } else if (xBoard.length == 5 || oBoard.length == 5) {
                 alert("Nobody win!");
                 return true;
             }
